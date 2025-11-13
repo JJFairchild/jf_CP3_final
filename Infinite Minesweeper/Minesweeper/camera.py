2042afb8d1c4b473817c2a346930a910bf65e289
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-11-13 11:49:10 -0700

    Infinite Minesweeper #4

---
+++ Infinite Minesweeper/Minesweeper/camera.py
@@ -0,0 +1,19 @@
+class Camera:
+    """Calculates where the screen should be displayed from coordinates in the minesweeper game"""
+    def __init__(self):
+        """Initializes values that the camera uses for coordinate calculation"""
+        self.x = 0 # OFFSET THESE TO CENTER ON SCREEN
+        self.y = 0
+        self.zoomscale = 0 # SET THIS TO APPROPRIATE VALUE
+
+    def wtos(self, x,y):
+        pass
+
+    def stow(self, x,y):
+        pass
+
+    def pan(self):
+        pass
+
+    def zoom(self):
+        pass
