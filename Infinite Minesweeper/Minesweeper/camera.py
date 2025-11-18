9b6d9fa6bc531daf55b1fbf481aa2234cf4e84b2
JJFairchild <jonas.fairchild@ucas-edu.net>
2025-11-18 11:50:10 -0700

    Minesweeper #7

--- Infinite Minesweeper/Minesweeper/camera.py
+++ Infinite Minesweeper/Minesweeper/camera.py
@@ -1,19 +1,24 @@
+import math
+
 class Camera:
     """Calculates where the screen should be displayed from coordinates in the minesweeper game"""
     def __init__(self):
         """Initializes values that the camera uses for coordinate calculation"""
         self.x = -600
         self.y = -600
-        self.zoomscale = 100 # Size of an individual tile in pixels
+        self.tile_size = 100
+        self.spacing = self.tile_size*9/8
 
     def wtos(self, x,y):
         pass
 
     def stow(self, x,y):
-        pass
+        pos = ((x+self.x)/self.spacing, (y+self.y)/self.spacing)
+        return (math.floor(pos[0]), math.floor(pos[1]))
 
-    def pan(self):
-        pass
+    def pan(self, mouse, new_mouse):
+        self.x += (mouse[0] - new_mouse[0])
+        self.y += (mouse[1] - new_mouse[1])
 
     def zoom(self):
         pass
--- test.py
+++ test.py
@@ -7,11 +7,14 @@ screen = pygame.display.set_mode((1200, 1200))
 tile_size = 100
 spacing = tile_size*9/8
 offset = [0,0]
+first_frame = True
 drag = False
+scroll = False
 mouse = pygame.mouse.get_pos()
 
+
 def stow(pos):
-    pos = (pos[0]/(spacing), pos[1]/(spacing))
+    pos = ((mouse[0]+offset[0])/(spacing), (mouse[1]+offset[1])/(spacing))
     return (math.floor(pos[0]), math.floor(pos[1]))
 
 running = True
@@ -23,29 +26,41 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            print(stow(mouse))
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                print(stow(mouse))
+            if event.button == 3:
+                drag = True
 
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
-            drag = True
+        if event.type == pygame.MOUSEWHEEL:
+            scroll = True
+            if event.y > 0:
+                tile_size *= 9/8
+                spacing = tile_size*9/8
+            elif event.y < 0:
+                tile_size /= 9/8
+                spacing = tile_size*9/8
 
         if event.type == pygame.MOUSEBUTTONUP and event.button == 3:
             drag = False
 
     new_mouse = pygame.mouse.get_pos()
-    if drag and mouse != new_mouse:
+    if (drag and mouse != new_mouse) or first_frame or scroll:
         # Drag movement
-        offset[0] += (new_mouse[0] - mouse[0])
-        offset[1] += (new_mouse[1] - mouse[1])
+        offset[0] += (mouse[0] - new_mouse[0])
+        offset[1] += (mouse[1] - new_mouse[1])
         mouse = new_mouse
 
+        scroll = False
+        print(offset)
+        
+
         # Draw screen
-        offset[0] += (pygame.mouse.get_pos()[0]-mouse[0])
-        offset[1] += (pygame.mouse.get_pos()[1]-mouse[1])
         screen.fill((50,50,50))
         for x in range(math.floor(1200/(spacing))+2):
             for y in range(math.floor(1200/(spacing))+2):
-                pygame.draw.rect(screen, (200,200,200), (x*spacing + offset[0]%(spacing) - spacing,y*spacing + offset[1]%(spacing) - spacing, tile_size, tile_size))
+                pygame.draw.rect(screen, (200,200,200), (x*spacing - offset[0]%spacing, y*spacing - offset[1]%spacing, tile_size, tile_size))
 
     pygame.display.flip()
     mouse = pygame.mouse.get_pos()
+    first_frame = False
